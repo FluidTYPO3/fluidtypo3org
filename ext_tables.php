bc9d4949b87c4d9fd84ea428ffda2e176df6c38f
Claus Due <claus@namelesscoder.net>
2020-09-22 14:23:28 +0200

    [TASK] Replace $_EXTKEY with extension key string

--- ext_tables.php
+++ ext_tables.php
@@ -3,6 +3,6 @@ if (!defined('TYPO3_MODE')) {
 	die ('Access denied.');
 }
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript', 'Provider extension for pages content');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('fluidtypo3org', 'Configuration/TypoScript', 'Provider extension for pages content');
 \FluidTYPO3\Flux\Core::registerProviderExtensionKey('FluidTYPO3.Fluidtypo3org', 'Page');
 \FluidTYPO3\Flux\Core::registerProviderExtensionKey('FluidTYPO3.Fluidtypo3org', 'Content');
