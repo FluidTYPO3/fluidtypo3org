5b0ebc9a67d9889fc0d8ffcb89ae79a0252f1966
Claus Due <claus@namelesscoder.net>
2020-09-22 14:27:20 +0200

    [TASK] Fix SQL schema definitions

--- ext_tables.sql
+++ ext_tables.sql
@@ -5,7 +5,7 @@ CREATE TABLE irc (
 	uid int(11) NOT NULL auto_increment,
 	pid int(11) DEFAULT '0' NOT NULL,
 	tstamp int(11) DEFAULT '0' NOT NULL,
-	filename varchar(255) DEFAULT '' NOT NULL
+	filename varchar(255) DEFAULT '' NOT NULL,
 
 	PRIMARY KEY (uid)
 );
@@ -17,7 +17,7 @@ CREATE TABLE viewhelper (
 	uid int(11) NOT NULL auto_increment,
 	pid int(11) DEFAULT '0' NOT NULL,
 	tstamp int(11) DEFAULT '0' NOT NULL,
-	filename varchar(255) DEFAULT '' NOT NULL
+	filename varchar(255) DEFAULT '' NOT NULL,
 
 	PRIMARY KEY (uid)
 );
